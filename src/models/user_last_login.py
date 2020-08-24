06695c8bc4404c0bf6b90aba5193ea029fa0f196
coolkapil <coolkaps918@gmail.com>
2020-08-24 18:06:49 +0530

    First commit

---
+++ src/models/user_last_login.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from mongoengine import Document, DateTimeField, ReferenceField
+from models import User
+
+
+class UserLastLogin(Document):
+    datetime = DateTimeField(required=True, default=datetime.now())
+    user = ReferenceField(User, required=True, unique=True)
